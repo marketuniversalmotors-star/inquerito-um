6bfce2a0e8edd148363776b2a092551c63d8e5ed
marketuniversalmotors-star <marketuniversalmotors@gmail.com>
2026-09-07 17:09:06 +0100

    Update script-index.js

--- script-index.js
+++ script-index.js
@@ -2,19 +2,16 @@ const urlGoogleScript = 'https://script.google.com/macros/s/AKfycbz-KotxTxPs_lZO
 
 function submitRating(score) {
     if (score <= 3) {
-        // Esconde os botões e mostra o aviso de carregamento
         document.getElementById('survey-section').style.display = 'none';
         document.getElementById('loading-msg').style.display = 'block';
         
         const formData = new FormData();
         formData.append('score', score);
         
-        // Envia para o Excel e avança para o agradecimento
         fetch(urlGoogleScript, { method: 'POST', body: formData })
         .then(() => window.location.href = 'agradecimento.html')
         .catch(err => console.error(err));
     } else {
-        // Redireciona para o inquérito e envia a nota no URL
         window.location.href = 'inquerito.html?score=' + score;
     }
 }

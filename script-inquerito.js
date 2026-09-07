f332612dccc7936b88b5f8fdb6743adec640352d
marketuniversalmotors-star <marketuniversalmotors@gmail.com>
2026-09-07 17:46:59 +0100

    Update script-inquerito.js

--- script-inquerito.js
+++ script-inquerito.js
@@ -1,6 +1,10 @@
 const form = document.getElementById('surveyForm');
 const urlGoogleScript = 'https://script.google.com/macros/s/AKfycbz-KotxTxPs_lZOYBPU27kATinLz4YI8omrKQwWlxN2vPGqiRBjF1aDU0AJQ_V3VyiT/exec';
 
+const urlParams = new URLSearchParams(window.location.search);
+const scoreDado = urlParams.get('score');
+const tokenDado = urlParams.get('token');
+
 if (form) {
     form.addEventListener('submit', e => {
         e.preventDefault(); 
@@ -10,11 +14,11 @@ if (form) {
         
         const formData = new FormData(form);
         
-        const urlParams = new URLSearchParams(window.location.search);
-        const scoreDado = urlParams.get('score');
-        
         if (scoreDado) {
             formData.append('score', scoreDado);
+        }
+        if (tokenDado) {
+            formData.append('token', tokenDado);
         }
         
         fetch(urlGoogleScript, { 
